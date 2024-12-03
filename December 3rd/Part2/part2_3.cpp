f154dbcf275e095d2ec7cafa5518bd28af93020f
Adrian Anthony <114228762+SejDreng@users.noreply.github.com>
2024-12-03 23:16:41 +0100

    3rd of December completed

--- December 3rd/Part2/part2_3.cpp
+++ December 3rd/Part2/part2_3.cpp
@@ -10,9 +10,9 @@
 int part2(){
     std::string line;
     std::ifstream file("input.txt");
-    std::regex pattern1(R"((mul\((\d+),(\d+)\))|(^don't\(\)$)|(^do\(\)$))");
+    std::regex pattern1(R"(don't\(\)|mul\((\d+),(\d+)\)|do\(\))");
+    
     int tot = 0;
-
     bool do_flag = true;
     bool dont_flag = false;
 
@@ -23,18 +23,16 @@ int part2(){
         while (it_1 != it_end){
 
             std::smatch match_1 = *it_1;
-        
-            std::cout << "Matched: " << match_1[0].str() << std::endl;
-            std::cout << "Matched1: " << match_1[1].str() << std::endl;
-            if(match_1.size() == 1){
-                if(match_1[0].str() == "do()"){
-                    do_flag = true;
-                    dont_flag = false;
-                }
-                else if(match_1[0].str() == "dont()"){
-                    do_flag = false;
-                    dont_flag = true;
-                }
+
+            if(match_1[0] == "do()"){
+                do_flag = true;
+                dont_flag = false;
+                it_1++;
+                continue;
+            }
+            else if(match_1[0] == "don't()"){
+                do_flag = false;
+                dont_flag = true;
                 it_1++;
                 continue;
             }
@@ -44,10 +42,8 @@ int part2(){
                     int num2 = std::stoi(match_1[2].str());
                     tot += num1 * num2;
                 } catch (const std::invalid_argument& e) {
-                    // Handle the error, e.g., log it or skip the invalid input
                     std::cerr << "Invalid argument: " << e.what() << std::endl;
                 } catch (const std::out_of_range& e) {
-                    // Handle the error, e.g., log it or skip the out-of-range input
                     std::cerr << "Out of range: " << e.what() << std::endl;
                 }
             }
